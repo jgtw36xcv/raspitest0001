afb3b67df278ef960302113f9848efa90a4554c9
seito2 <seito2@mechatro.local>
2020-11-20 13:24:17 +0900

    add code

--- testjoystick.c
+++ testjoystick.c
@@ -11,34 +11,33 @@
 #define JOY_DEV "/dev/input/js0"
 
 int main(void)
-{	int fd, Socket, *axis=NULL, num_of_axis=0, num_of_buttons=0, x, fl = 1;
-	char *button=NULL, temstr[80], pstr[256];
+{	int fd, Socket, fl = 1;
+	char pstr[256];
 	struct js_event js;
 	struct sockaddr_in serverSockAddr;
 	unsigned short serverPort = 12479;
-	unsigned int sockAddrLen;
+//	unsigned int sockAddrLen;
 
-	sockAddrLen = sizeof(serverSockAddr);
+//	sockAddrLen = sizeof(serverSockAddr);
 
 	memset(&serverSockAddr, 0, sizeof(serverSockAddr));
 
 	serverSockAddr.sin_family = AF_INET;
-	if(inet_aton("127.0.0.1", &serverSockAddr.sin_addr) == 0)
-	{	perror("inet_aton() failed.");
-		exit(-1);
-	}
+	inet_aton("127.0.0.1", &serverSockAddr.sin_addr);
 	serverSockAddr.sin_port = htons(serverPort);
 
-	while(1)
-	{	if((Socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
-		{	perror("socket() failed.");
-			close(Socket);
-			continue;
-		}
+	if((Socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+	{	perror("socket() failed.");
+		close(Socket);
+		free(serverSockAddr);
+		exit(-1);
+	}
 
+
+	while(1)
+	{	
 		if(connect(Socket, (struct sockaddr*) &serverSockAddr, sizeof(serverSockAddr)) < 0)
-		{	perror("connect() failed.");
-			close(Socket);
+		{	close(Socket);
 			continue;
 		}
 
@@ -77,12 +76,11 @@ int main(void)
 					strcat(pstr, temstr);
 				}
 
-				strcat(pstr, "B  ");
-
 				for(x = 0; x < num_of_buttons; x++)
 				{	sprintf(temstr, "%d:%s ", x, button[x] == 1 ? "on " : "off");
 					strcat(pstr, temstr);
 				}
+
 				if(send(Socket, pstr, (size_t) strlen(pstr)+1, 0) != strlen(pstr)+1)
 				{	perror("send() failed.");
 					fl = 0;
